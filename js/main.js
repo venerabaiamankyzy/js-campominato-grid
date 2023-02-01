b6b0f5ebda87a6d1488755fe1d73eef6fc83ad43
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-02-01 17:19:25 +0100

    + numbers on .square

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
  *********************************/
 
 const gridEl = document.getElementById("grid");
-generatedGrid(gridEl);
+const gridDimensionEl = 100;
+generatedGrid(gridEl, gridDimensionEl);
 
 /**********************************
  *                                *
@@ -13,10 +14,25 @@ generatedGrid(gridEl);
  *                                *
  *********************************/
 
-function generatedGrid(grid) {
-  for (let i = 0; i < 100; i++) {
+function generatedGrid(grid, dimension) {
+  //numero dei square e per 100 volte sono aggiunti
+  for (let i = 0; i < dimension; i++) {
+    const numberOfSquare = i + 1;
+    //genero un div
     const squareEl = document.createElement("div");
+
+    // gli aggiungo la classe .square nella grid
     squareEl.classList.add("square");
-    gridEl.append(squareEl);
+
+    //gli aggiungo i numeri
+    squareEl.innerHTML = numberOfSquare;
+
+    // aggiungo un addeventlistener sul click che faccia il "toggle" della classe .active
+    squareEl.addEventListener("click", function () {
+      this.classList.toggle("active");
+    });
+
+    //lo aggiungo alla griglia
+    grid.append(squareEl);
   }
 }
